04c6a51303c27fe7bf61a9322fffd2860cc14463
soumendradas <31432889+soumendradas@users.noreply.github.com>
2021-09-29 23:39:04 +0530

    Email Integration

--- src/main/java/in/nareshit/raghu/controller/DoctorController.java
+++ src/main/java/in/nareshit/raghu/controller/DoctorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,134 +20,135 @@ import in.nareshit.raghu.entity.Doctor;
 import in.nareshit.raghu.exception.DoctorNotFoundException;
 import in.nareshit.raghu.service.IDoctorService;
 import in.nareshit.raghu.service.ISpecializationService;
+import in.nareshit.raghu.util.MyMailUtil;
 import in.nareshit.raghu.view.DoctorExcelView;
 
 @Controller
 @RequestMapping("doctor")
 public class DoctorController {
-	
+
 	@Autowired
 	private IDoctorService service;
-	
+
 	@Autowired
 	private ISpecializationService specService;
-	
+
+	@Autowired
+	private MyMailUtil mailUtil;
+
 	private void createDynamicUi(Model model) {
 		Map<Long, String> specializtions = specService.getSpecIdAndName();
 		model.addAttribute("specializations", specializtions);
 	}
-	
+
 	@GetMapping("register")
-	public String showRegister(
-			@RequestParam(value = "message", required = false) String message,
-			Model model) {
-		
+	public String showRegister(@RequestParam(value = "message", required = false) String message, Model model) {
+
 		model.addAttribute("message", message);
 		createDynamicUi(model);
-		
+
 		return "DoctorRegister";
 	}
-	
+
 	@PostMapping("/save")
-	public String save(@ModelAttribute Doctor doctor,
-			RedirectAttributes redirectAttributes) {
+	public String save(@ModelAttribute Doctor doctor, RedirectAttributes redirectAttributes) {
 		Long id = service.saveDoctor(doctor);
-		redirectAttributes.addAttribute("message", "Doctor '"+id+"' created");
-		
+		String message = "Doctor '" + id + "' created";
+		redirectAttributes.addAttribute("message", message);
+
+		if (id != null) {
+			new Thread(() -> {
+				mailUtil.send(doctor.getEmail(), "Doctor Id creation Success", message,
+						new ClassPathResource("/static/myRes/demo.pdf"));
+
+			}).start();
+		}
+
 		return "redirect:register";
 	}
-	
+
 	@GetMapping("all")
-	public String showAll(Model model,
-			@RequestParam(value = "message", required = false) String message) {
+	public String showAll(Model model, @RequestParam(value = "message", required = false) String message) {
 		List<Doctor> list = service.getAllDoctors();
 		model.addAttribute("list", list);
 		model.addAttribute("message", message);
-		
+
 		return "DoctorData";
 	}
-	
+
 	@GetMapping("delete")
-	public String delete(@RequestParam Long id,
-			RedirectAttributes attributes) {
+	public String delete(@RequestParam Long id, RedirectAttributes attributes) {
 		String message = "";
 		try {
 			service.removeDoctor(id);
-			message = "Doctor "+id+" deleted";
-		}catch (DoctorNotFoundException e) {
+			message = "Doctor " + id + " deleted";
+		} catch (DoctorNotFoundException e) {
 			message = e.getMessage();
 		}
 		attributes.addAttribute("message", message);
 		return "redirect:all";
 	}
-	
+
 	@GetMapping("edit")
-	public String showEditPage(@RequestParam Long id, 
-			Model model,
-			RedirectAttributes attributes) {
+	public String showEditPage(@RequestParam Long id, Model model, RedirectAttributes attributes) {
 		String page = "";
 		try {
 			Doctor doctor = service.getOneDoctor(id);
 			model.addAttribute("doctor", doctor);
 			createDynamicUi(model);
 			page = "DoctorEdit";
-			
-		}catch (DoctorNotFoundException e) {
+
+		} catch (DoctorNotFoundException e) {
 			attributes.addAttribute("message", e.getMessage());
 			page = "redirect:all";
 		}
-		
-		
+
 		return page;
 	}
-	
+
 	@PostMapping("/update")
-	public String update(@ModelAttribute Doctor doctor,
-			RedirectAttributes attributes){
-		
+	public String update(@ModelAttribute Doctor doctor, RedirectAttributes attributes) {
+
 		String message = "";
 		try {
 			service.updateDoctor(doctor);
-			message = "Doctor "+doctor.getId()+ " updated";
-		}catch (DoctorNotFoundException e) {
+			message = "Doctor " + doctor.getId() + " updated";
+		} catch (DoctorNotFoundException e) {
 			message = e.getMessage();
 		}
 		attributes.addAttribute("message", message);
 		return "redirect:all";
 	}
-	
-	//-------------------------------------------------------------------------------
+
+	// -------------------------------------------------------------------------------
 	@GetMapping("checkEmail")
 	@ResponseBody
-	public String validateEmail(@RequestParam("email")String email,
-			@RequestParam("id") Long id) {
+	public String validateEmail(@RequestParam("email") String email, @RequestParam("id") Long id) {
 		String message = "";
-		if(service.isEmailExist(email, id)) {
-			message ="<b>"+ email + "</b> is exist";
+		if (service.isEmailExist(email, id)) {
+			message = "<b>" + email + "</b> is exist";
 		}
 		return message;
 	}
-	
+
 	@GetMapping("checkMobile")
 	@ResponseBody
-	public String validateMobile(@RequestParam("mobile") String mobile, 
-			@RequestParam("id") Long id) {
+	public String validateMobile(@RequestParam("mobile") String mobile, @RequestParam("id") Long id) {
 		String message = "";
-		if(service.isMobileExist(mobile, id)) {
-			message = "<b>"+mobile+"</b> is exist";
+		if (service.isMobileExist(mobile, id)) {
+			message = "<b>" + mobile + "</b> is exist";
 		}
 		return message;
 	}
-	
+
 	@GetMapping("/excel")
 	public ModelAndView exportToExcel() {
 		ModelAndView mv = new ModelAndView();
 		mv.setView(new DoctorExcelView());
 		List<Doctor> doctors = service.getAllDoctors();
 		mv.addObject("doctors", doctors);
-		
+
 		return mv;
 	}
-	
 
 }
---
+++ src/main/java/in/nareshit/raghu/util/MyMailUtil.java
@@ -0,0 +1,84 @@
+package in.nareshit.raghu.util;
+
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyMailUtil {
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	public boolean send(
+			String to[],
+			String cc[],
+			String bcc[],
+			String subject,
+			String text,
+			Resource files[]) {
+		
+		boolean sent = false;
+		
+		try {
+			
+			//1. Create new empty message object
+			MimeMessage message = mailSender.createMimeMessage();
+			
+			//2. fill details (message, attachmentExist)
+			MimeMessageHelper helper = new MimeMessageHelper(message, 
+					files!=null && files.length > 0);
+			
+			helper.setTo(to);
+			if(cc!=null) {
+				helper.setCc(cc);
+			}
+			
+			if(bcc != null) {
+				helper.setBcc(bcc);
+			}
+			helper.setSubject(subject);
+			helper.setText(text);
+			
+			if(files != null) {
+				for(var rob: files) {
+					helper.addAttachment(rob.getFilename(), rob);
+				}
+			}
+			mailSender.send(message);
+			
+			sent = true;
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			sent = false;
+		}
+		
+		return sent;
+	}
+	
+	// overloaded methods
+	public boolean send(
+			String to,
+			String subject,
+			String text,
+			Resource files) {
+		
+		return send(new String[] {to}, 
+				null, null, 
+				subject, text,
+				files!=null?new Resource[] {files}:null);
+	}
+	
+	public boolean send(
+			String to,
+			String subject,
+			String text) {
+		return send(to, subject, text, null);
+	}
+
+}
